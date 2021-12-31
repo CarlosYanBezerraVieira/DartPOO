98446e21c9268492004c025b3fe0f25c1adcd469
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-12-31 16:48:24 -0300

    self promotion

--- dart_poo/lib/8_interfaces/a_interfaces.dart
+++ dart_poo/lib/8_interfaces/a_interfaces.dart
@@ -2,15 +2,22 @@ import 'package:dart_poo/8_interfaces/carro.dart';
 import 'package:dart_poo/8_interfaces/gol.dart';
 import 'package:dart_poo/8_interfaces/uno.dart';
 
+Carro carroGol = Gol();
+// variaveis de nivel superior e atributos de classe não não auto promovidas
 void main() {
   var uno = Uno();
   var gol = Gol();
+  print(gol.tipoDeRodas);
   Carro carroGol = Gol();
-
+  //quando checamos se a variavel é de um tipo, caso ela seja o dart vai autonaticamente converte essa instancia
+  if (carroGol is Gol) {
+    carroGol.tipoDeRodas();
+  }
+  //var tiposDeRodas = ((carroGol as Gol).tipoDeRodas());
+  //print("tipo de roda $tiposDeRodas");
   printDadosCarro(uno);
   printDadosCarro(gol);
   // carroGol.  não ten  tipo  de rodas
-  print(gol.tipoDeRodas);
 }
 
 void printDadosCarro(Carro carro) {
@@ -21,5 +28,6 @@ void printDadosCarro(Carro carro) {
     Rodas: ${carro.rodas}
     Motor: ${carro.motor} 
     Velocidade máxima ${carro.velocidadeMaxima()}
+    Tipo de roda: ${carro is Gol ? carro.tipoDeRodas() : "não disponivel"}
   """);
 }
