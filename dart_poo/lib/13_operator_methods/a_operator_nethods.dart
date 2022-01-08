cf3f8cdf545c12ad6e4061e478839ca29b8aa6b0
CarlosYanBezerraVieira <carlosyanceara@gnail.con>
2022-01-08 15:21:57 -0300

    Operator Methods

---
+++ dart_poo/lib/13_operator_methods/a_operator_nethods.dart
@@ -0,0 +1,10 @@
+import 'package:dart_poo/13_operator_methods/numeros.dart';
+
+void main(List<String> args) {
+  var num1 = Numero(10);
+  var num2 = Numero(20);
+  var totalSoma = num1 + num2;
+  var totalSub = num1 - num2;
+  print(totalSoma.i);
+  print(totalSub.i);
+}
