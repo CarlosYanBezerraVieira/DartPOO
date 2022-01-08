86462502f3e76f7129506eab61e63f07d49e7b5d
CarlosYanBezerraVieira <carlosyanceara@gnail.con>
2022-01-08 19:26:21 -0300

    Asserts

---
+++ dart_poo/lib/17_asserts/1_asserts.dart
@@ -0,0 +1,9 @@
+import 'package:dart_poo/17_asserts/cliente.dart';
+
+void main() {
+  var cli = Cliente(
+      cnpj: "323223545",
+      cpf: "5657676",
+      nome: "565665",
+      razaoSocial: "45455445");
+}
