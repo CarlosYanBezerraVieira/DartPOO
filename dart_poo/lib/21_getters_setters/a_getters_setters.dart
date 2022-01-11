4306ae8cc70ca1819398d9b929cd8bce2cfaa188
CarlosYanBezerraVieira <carlosyanceara@gnail.con>
2022-01-11 10:46:48 -0300

    Getters e Setters

--- dart_poo/lib/21_getters_setters/a_getters_setters.dart
+++ dart_poo/lib/21_getters_setters/a_getters_setters.dart
@@ -1,10 +1,22 @@
 void main(List<String> args) {
   var pessoa = Pessoa();
-  pessoa.setNome("Carlos");
+  pessoa.nome = "Carlos";
+  // não unsado pessoa.setNome("Carlos");
+  print(pessoa.nome);
 }
 
 class Pessoa {
   String? _nome;
-  String? getNome() => _nome;
-  void setNome(String nome) => _nome = nome;
+  //não é muito usado no dart
+//  String? getNome() => _nome;
+  //void setNome(String nome) => _nome = nome;
+
+//usado
+
+  String? get nome => _nome;
+  set nome(String? nome) {
+    if (nome != null && nome.length > 3) {
+      _nome = nome;
+    }
+  }
 }
